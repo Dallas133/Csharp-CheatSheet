b42c0a0fbde0bc220bb8b22725681912a29ebe42
Dallas Carraher <dall133android@gmail.com>
2017-05-12 13:08:31 -0700

    initial upload

---
+++ Csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            
+            bool canVote;
+
+            for (int i = 0; i < args.Length; i++){
+                Console.WriteLine(args[i]);  
+                }
+        }
+    }
+
+}
