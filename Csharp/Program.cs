2cdfe47d31d890086f09d07539fba27d5a5dd788
Dallas Carraher <dall133android@gmail.com>
2017-05-12 15:08:34 -0700

    updates

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -6,15 +6,34 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+			/*  Basic Write and Read commands
             Console.Write("What is your name?: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
-            
-            bool canVote;
+            */
 
-            for (int i = 0; i < args.Length; i++){
+			/* Basic data types
+            bool canVote = true; //boolean type t/f
+
+            char grade = 'A'; //character type
+
+            int maxInt = int.MaxValue; //integer with a max value of 2.1 billion
+
+            long maxLong = long.MaxValue; //long with a max value of 9.2 quintilion
+
+            decimal maxDec = decimal.MaxValue; //decimal with a max value of 79.2 octilion
+
+            float maxFloat = float.MaxValue; //float is a 32bit number with max value 3.4x10^38 with 7 decimal precision
+            
+            double maxDouble = double.MaxValue; //double is a 32bit number with a max value of 1.79x10^308 with 15 decimals of precision.
+
+            
+            */
+
+
+			for (int i = 0; i < args.Length; i++){
                 Console.WriteLine(args[i]);  
-                }
+            }
         }
     }
 
