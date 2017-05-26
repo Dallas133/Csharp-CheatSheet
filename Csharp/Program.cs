e1bc1f58d63fc96614b6cbafd029e4bd43f3cf17
DallasCarraher <dall133android@gmail.com>
2017-05-25 17:40:38 -0700

    started list documentation

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -247,16 +247,51 @@ namespace Csharp
             }
             
             */
+            
+            /*
 
-
-            //Lists (similar to vectors; array resizes FOR you
+            //Lists (similar to vectors in C++; array resizes FOR you
 
             List<int> numList = new List<int>(); //how to declare a list
 
             numList.Add(5);
             numList.Add(15);
             numList.Add(25);
-      
+
+            int[] randArray = { 1, 2, 3, 4, 5 };
+            numList.AddRange(randArray);
+
+            List<int> numList2 = new List<int>(randArray);
+
+            List<int> numList3 = new List<int>(new int[] { 1, 2, 3 });
+            
+            numList.Insert(1, 10);
+
+            numList.Remove(5);
+            
+            numList.RemoveAt(2);
+
+            for (int i = 0; i < numList.Count; i++){
+                Console.WriteLine(numList[i]);
+                Console.WriteLine("-");
+            }
+
+            Console.WriteLine("4 is in index " + numList3.IndexOf(4));
+
+            Console.WriteLine("5 in List " + numList.Contains(5));
+
+            List<string> strList = new List<string>(new string[] { "Tom", "Paul" });
+            
+            Console.WriteLine("Tom in list" + strList.Contains("tom",
+                StringComparer.OrdinalIgnoreCase));
+
+            strList.Sort();
+            
+            */
+            
+            
+    
+                
         }
     }
 
